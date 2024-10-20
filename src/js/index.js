f3d6e2635406d192c8ef41169fecb1fbbdf637ff
Parava <P.arava@mail.ru>
2024-10-20 12:50:43 +0500

    разбираюсь как добавить ссылку на гитхаб падже

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
 import "./import/modules";
 import "./import/components";
+
+// слайдер
+new Swiper('.slider-review',{
+    grabCursor: true,
+    spaceBetween: 70,
+    // centeredSlides: true,
+    loop: true,
+    // loopedSlides: 1,
+    // почемуто не работает в правую сторону
+    centeredSlides: true,
+    slidesPerView: 2.5,
+    speed: 500,
+});
+// всплывающая форма
